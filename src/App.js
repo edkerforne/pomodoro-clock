ccfbbd4de02de35db05825267e3251d4d682cde1
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-02-08 02:22:21 +0100

    App styling and layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-wrapper">
+      <main id="display">
+        <h1 id="status">Work time!</h1>
+        <div id="counter">25:00</div>
+        <div id="timer-controls">
+          <button class="timer__control reset">RESET</button>
+          <button class="timer__control start">RESUME</button>
+        </div>
+      </main>
+
+      <div id="options">
+        <div>Session length:</div>
+        <div class="option-controls">
+          <button class="option__control left">-</button>
+          <input type="text" class="option__count" defaultValue="25"></input>
+          <button class="option__control right">+</button>
+        </div>
+
+        <div>Break length:</div>
+        <div class="option-controls">
+          <button class="option__control left">-</button>
+          <input type="text" class="option__count" defaultValue="5"></input>
+          <button class="option__control right">+</button>
+        </div>
+      </div>
+
+      <footer id="author">
+        <span>
+          by <a href="https://edkerforne.github.io/">Edwin Kerforne</a>
+        </span>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
