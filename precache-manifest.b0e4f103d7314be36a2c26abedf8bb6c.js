be64c61659a67c419dcf79f47962c56ceb34073b
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-02-09 20:25:00 +0100

    Updates

---
+++ precache-manifest.b0e4f103d7314be36a2c26abedf8bb6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecd2ebfa5535f74240c434cb8847317",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "fd1ae1bc4c1d267b8ef7",
+    "url": "/pomodoro-clock/static/css/main.d40d5116.chunk.css"
+  },
+  {
+    "revision": "b54d94c5641963e39132",
+    "url": "/pomodoro-clock/static/js/2.5eea9d20.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/pomodoro-clock/static/js/2.5eea9d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1ae1bc4c1d267b8ef7",
+    "url": "/pomodoro-clock/static/js/main.e4ac596d.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  },
+  {
+    "revision": "8037aba19f5048c14fad87ed8e538195",
+    "url": "/pomodoro-clock/static/media/beep.8037aba1.wav"
+  }
+]);
